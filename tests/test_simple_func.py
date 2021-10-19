32b9308996072958fb22b460fb895419b163ec5b
Paulina <pobar.learn@gmail.com>
2021-10-19 22:56:47 +0100

    Modification

--- tests/test_simple_func.py
+++ tests/test_simple_func.py
@@ -3,7 +3,7 @@ from pricer.simple_func import simple_fn
 
 def test_simple_fn():
     """simple test"""
-    assert simple_fn(4)==4
+    assert simple_fn(4) == 6
 
 # if __name__=="__main__":
 #     test_simple_fn()
